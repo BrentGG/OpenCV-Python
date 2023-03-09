eb55de54ef5f02bc10625a9b4eff1c26316f4b59
brent <brent.gerets@gmail.com>
2023-03-09 17:25:08 +0100

    adjusted window size

--- CloudyWithAChanceOfMeatballs.py
+++ CloudyWithAChanceOfMeatballs.py
@@ -1,26 +1,36 @@
 import cv2 as cv
+import imutils
+import ctypes
 
 if __name__ == '__main__':
-    # get video capture
+    # Get screen info
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
+    # Get video capture
     cap = cv.VideoCapture(1)
     if not cap.isOpened():
         print("Could not open camera")
         exit(-1)
 
-    # get the facial detection cascade
+    # Get the facial detection cascade
     faceCascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_alt.xml")
 
     while True:
+        # Read frame and detect face
         success, img = cap.read()
+        img = imutils.resize(img, height=int(screensize[1] * 0.8))
         imgGrey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         imgGrey = cv.equalizeHist(imgGrey)
         faces = faceCascade.detectMultiScale(imgGrey, 1.3, 5)
 
+        # Draw rectangle around face
         for (x, y, w, h) in faces:
             img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv.imshow('Cloudy With A Chance Of Meatballs', img)
 
-        # close when q is pressed
+        # Close when q is pressed
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
