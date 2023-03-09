199bf18db2694c1ba0c82633c653fb6d207ccc62
brent <brent.gerets@gmail.com>
2023-03-09 18:05:47 +0100

    let it rain meatballs

--- CloudyWithAChanceOfMeatballs.py
+++ CloudyWithAChanceOfMeatballs.py
@@ -1,8 +1,17 @@
 import cv2 as cv
 import imutils
 import ctypes
+import time
+
+from Meatball import Meatball
 
 if __name__ == '__main__':
+    # Game data
+    score = 0
+    meatballs = []
+    meatballSpawnRate = 1
+    meatballSpawned = time.time()
+
     # Get screen info
     user32 = ctypes.windll.user32
     user32.SetProcessDPIAware()
@@ -20,6 +29,7 @@ if __name__ == '__main__':
     while True:
         # Read frame and detect face
         success, img = cap.read()
+        img = cv.flip(img, 1)
         img = imutils.resize(img, height=int(screensize[1] * 0.8))
         imgGrey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         imgGrey = cv.equalizeHist(imgGrey)
@@ -28,6 +38,25 @@ if __name__ == '__main__':
         # Draw rectangle around face
         for (x, y, w, h) in faces:
             img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Update game
+        i = 0
+        while i < len(meatballs):
+            meatballs[i].move()
+            if meatballs[i].getPosition()[1] > len(img) + meatballs[i].getRadius():
+                meatballs.pop(i)
+                i -= 1
+                print("removed")
+            else:
+                img = cv.circle(img, meatballs[i].getPosition(), meatballs[i].getRadius(), (0, 255, 0) if meatballs[i].isGood() else (0, 0, 255), cv.FILLED)
+            i += 1
+
+        if time.time() - meatballSpawned > meatballSpawnRate:
+            meatballs.append(Meatball(len(img[0])))
+            meatballSpawned = time.time()
+            print("added")
+
+        # Show frame
         cv.imshow('Cloudy With A Chance Of Meatballs', img)
 
         # Close when q is pressed
--- Meatball.py
+++ Meatball.py
@@ -3,12 +3,28 @@ import random
 
 class Meatball:
     radius = 10
+    remove = False
 
     def __init__(self, width):
         self.x = random.randint(0 + self.radius, width - self.radius)
         self.y = self.radius
         self.good = random.choice([True, False])
-        self.speed = random.randint(1, 5)
+        self.speed = random.randint(5, 10)
 
     def move(self):
         self.y += self.speed
+
+    def getPosition(self):
+        return self.x, self.y
+
+    def getRadius(self):
+        return self.radius
+
+    def isGood(self):
+        return self.good
+
+    def setRemove(self):
+        self.remove = True
+
+    def getRemove(self):
+        return self.remove
